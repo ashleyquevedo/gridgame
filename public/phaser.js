1aba852bd8ec26da99e98c55a9ceb34d19925c8b
Ashley Quevedo <ashleysquevedo@gmail.com>
2022-10-02 20:37:56 -0400

    edited socket.io config

--- public/phaser.js
+++ public/phaser.js
@@ -34,7 +34,7 @@ function preload() {
 }
 
 function create() {
-  this.socket = io();
+  this.socket = io("localhost:8080", { autoConnect: false });
   // io().on("connect", () => {
   //   console.log("Connected!");
   // });
