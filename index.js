d50de52e4a1711cfaacbd264177a3aa2b353485e
Ashley Quevedo <ashleysquevedo@gmail.com>
2023-04-21 16:44:16 -0400

    save

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.static("public"));
 // });
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + "/public/views/index.html");
 });
 
 app.get("/play", (req, res) => {
